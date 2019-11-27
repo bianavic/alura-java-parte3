a13278fc2dfe01d9d33ad704dff9eb28b5e56035
Assami <fabiana.assami@fvfx68qxhv22.wirecard.lan>
2019-11-27 15:38:30 -0300

    parte 04

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -6,7 +6,14 @@ public class ContaCorrente extends Conta {
 
     public ContaCorrente(int agencia, int numero) { // adicionamos 1 construtor esoecifico
         // com o super podemos chamar construtor padrao e especifico
-        super(agencia, numero); // criamos construtor ESPECIFICO - nao existe construtor padrao
+        super(agencia, numero);
+    }// criamos construtor ESPECIFICO - nao existe construtor padrao
 
-    }
+        // redefinir o comportamento deste metodo com override (@Override é uma a notacao)
+        @Override
+        public boolean saca(double valor){
+            double valorASacar = valor + 0.2;
+            return super.saca(valorASacar);
+        }
+
 }
--- bytebank-herdado-conta/src/ContaPoupanca.java
+++ bytebank-herdado-conta/src/ContaPoupanca.java
@@ -1,7 +1,6 @@
-
+// ainda nao existem codigos, apenas o CONSTRUTOR
 public class ContaPoupanca extends Conta {
     public ContaPoupanca(int agencia, int numero) {
         super(agencia, numero);
     }
-
 }
